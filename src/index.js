cd812321f2bb9bc4a320dff3ed50ad0737318ae8
oxyyyyy <elektronikaion@gmail.com>
2017-03-12 16:20:44 +0200

    Hm, probably need to save

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY ='AIzaSyAmnmk-THUlPxrDydaJ_uqqnmqp9VjFd3w';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'DIY' }, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos });
+    });
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
